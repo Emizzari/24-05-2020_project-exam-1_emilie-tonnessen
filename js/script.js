9689257ece95f4b8b5e13157872bac137bb3cc25
Emizzari <emilie@tonnessen.com>
2020-05-04 15:36:00 +0200

    Navigation

--- js/script.js
+++ js/script.js
@@ -10,3 +10,51 @@ function toogleMobileMenu() {
     }
 }
 
+
+// Mobile Menu Hide on scolling down - show on scrolling up
+var prevScrollpos = window.pageYOffset;
+
+
+window.onscroll = function () {
+
+    var currentScrollPos = window.pageYOffset;
+
+
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("mobile-scroll-menu").style.bottom = "0";
+    } else {
+        document.getElementById("mobile-scroll-menu").style.bottom = "-100px";
+    }
+
+
+    prevScrollpos = currentScrollPos;
+}
+
+
+
+// Tablet menu toogle
+function toogleTabletMenu() {
+    var tabletMenu = document.getElementById("tablet-menu");
+    var hamburgerIcon = document.getElementById("hamburger");
+    var crossIcon = document.getElementById("cross");
+
+
+    if (tabletMenu.style.right === "-250px") {
+        tabletMenu.style.right = "0";
+
+        hamburgerIcon.style.display = "none";
+        crossIcon.style.display = "block";
+    }
+    
+    
+    else {
+        tabletMenu.style.right = "-250px";
+
+        hamburgerIcon.style.display = "block";
+        crossIcon.style.display = "none";
+    }
+}
+
+
+
+
