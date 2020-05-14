54a4f20dbb3cee2989bf31b4808449b17beeaeac
Emizzari <emilie@tonnessen.com>
2020-05-14 11:44:22 +0200

    Renaming classes

--- js/rockets.js
+++ js/rockets.js
@@ -55,40 +55,42 @@ function displayRockets(rockets) {
                         }
                                      
         html += `       </h6>  
-                        <i class="fas fa-plus"></i>
-                        <i class="fas fa-minus"></i>
+                        <div class="dd-btn__icon grid__center">
+                            <div class="dd-btn__icon-1"></div>
+                            <div class="dd-btn__icon-2"></div> 
+                        </div>
                     </label>
-                        <div class="dd-content vehicle">
-                            <img class="vehicle-img" src="${rockets[i].flickr_images[i]}" alt="${rockets[i].rocket_name}">
-                            <div class="vehicle-wrap">
-                            <h5 class="vehicle-headline">technical overview</h5>
+                    <div class="dd-content vehicle">
+                        <img class="vehicle-img" src="${rockets[i].flickr_images[i]}" alt="${rockets[i].rocket_name}">
+                        <div class="dd-content__panel">
+                            <h5 class="dd-content__headline">technical overview</h5>
 
-                            <h6 class="vehicle-title">company</h6>
-                            <p class="vehicle-number">${rockets[i].company}</p>
+                            <h6 class="dd-content__title">company</h6>
+                            <p class="dd-content__description">${rockets[i].company}</p>
 
-                            <h6 class="vehicle-title">country</h6>
-                            <p class="vehicle-number">${rockets[i].country}</p>
+                            <h6 class="dd-content__title">country</h6>
+                            <p class="dd-content__description">${rockets[i].country}</p>
 
-                            <h6 class="vehicle-title">first flight</h6>
-                            <p class="vehicle-number">${rockets[i].first_flight}</p>
+                            <h6 class="dd-content__title">first flight</h6>
+                            <p class="dd-content__description">${rockets[i].first_flight}</p>
 
-                            <h6 class="vehicle-title">engine</h6>
-                            <p class="vehicle-number">${rockets[i].engines.type} ${rockets[i].engines.version}</p>
+                            <h6 class="dd-content__title">engine</h6>
+                            <p class="dd-content__description">${rockets[i].engines.type} ${rockets[i].engines.version}</p>
 
-                            <h6 class="vehicle-title">height</h6>
-                            <p class="vehicle-number">${rockets[i].height.meters} m</p>
+                            <h6 class="dd-content__title">height</h6>
+                            <p class="dd-content__description">${rockets[i].height.meters} m</p>
 
-                            <h6 class="vehicle-title">diameter</h6>
-                            <p class="vehicle-number">${rockets[i].diameter.meters} m</p>
+                            <h6 class="dd-content__title">diameter</h6>
+                            <p class="dd-content__description">${rockets[i].diameter.meters} m</p>
 
-                            <h6 class="vehicle-title">mass</h6>
-                            <p class="vehicle-number">${rockets[i].mass.kg} kg</p>
+                            <h6 class="dd-content__title">mass</h6>
+                            <p class="dd-content__description">${rockets[i].mass.kg} kg</p>
 
-                            <h6 class="vehicle-title">success rate</h6>
-                            <p class="vehicle-number">${rockets[i].success_rate_pct} %</p>
+                            <h6 class="dd-content__title">success rate</h6>
+                            <p class="dd-content__description">${rockets[i].success_rate_pct} %</p>
 
-                            <h6 class="vehicle-title">details</h6>
-                            <p class="vehicle-number">${rockets[i].description}</p>
+                            <h6 class="dd-content__title">details</h6>
+                            <p class="dd-content__description">${rockets[i].description}</p>
                        
                             <a class="wiki" href="${rockets[i].wikipedia}">wikipedia</a>
                         </div>                          
@@ -116,20 +118,20 @@ function displayRoadster(roadster) {
                     <div class="vehicle-wrap">
                         <h5 class="vehicle-headline">${roadster.name}</h5>
 
-                        <h6 class="vehicle-title">launch date</h6>
-                        <p class="vehicle-number">${convertDate(roadster.launch_date_unix)}</p>
+                        <h6 class="dd-content__title">launch date</h6>
+                        <p class="dd-content__description">${convertDate(roadster.launch_date_unix)}</p>
 
-                        <h6 class="vehicle-title">earth distance</h6>
-                        <p class="vehicle-number">${roadster.earth_distance_km} km</p>
+                        <h6 class="dd-content__title">earth distance</h6>
+                        <p class="dd-content__description">${roadster.earth_distance_km} km</p>
 
-                        <h6 class="vehicle-title">mars distance</h6>
-                        <p class="vehicle-number">${roadster.mars_distance_km} km</p>
+                        <h6 class="dd-content__title">mars distance</h6>
+                        <p class="dd-content__description">${roadster.mars_distance_km} km</p>
 
-                        <h6 class="vehicle-title">orbit type</h6>
-                        <p class="vehicle-number">${roadster.orbit_type}</p>
+                        <h6 class="dd-content__title">orbit type</h6>
+                        <p class="dd-content__description">${roadster.orbit_type}</p>
 
-                        <h6 class="vehicle-title">details</h6>
-                        <p class="vehicle-number">${roadster.details}</p>
+                        <h6 class="dd-content__title">details</h6>
+                        <p class="dd-content__description">${roadster.details}</p>
                                 
                         <a href='${roadster.video}'><div class="yt">YouTube Video</div></a>
                         <a href="${roadster.wikipedia}"><div class="wiki">Wikipedia</div></a>
--- js/sites.js
+++ js/sites.js
@@ -33,27 +33,29 @@ function displayLaunchPads(launchPads) {
                     <input type="checkbox" id="toggle_id-${i}" class="checkbox"/>
                     <label for="toggle_id-${i}" class="dd-btn">
                         <h6 class="dd-btn__title">${launchPads[i].name} - <span class="italic">${launchPads[i].status}</span></h6>  
-                        <i class="fas fa-plus"></i>
-                        <i class="fas fa-minus"></i>
+                        <div class="dd-btn__icon grid__center">
+                            <div class="dd-btn__icon-1"></div>
+                            <div class="dd-btn__icon-2"></div> 
+                        </div>
                     </label>
 
                     <div class="dd-content">
                     <iframe class="map" src="https://maps.google.com/maps?q=${launchPads[i].location.latitude},${launchPads[i].location.longitude}&hl=en&z=10&amp;output=embed" width="100%"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                        <div class="site-wrap">
-                            <h6 class="vehicle-title">${launchPads[i].name}</h6>
-                            <p class="vehicle-number">${launchPads[i].site_name_long}</p>
+                        <div class="dd-content__panel">
+                            <h6 class="dd-content__title">${launchPads[i].name}</h6>
+                            <p class="dd-content__description">${launchPads[i].site_name_long}</p>
 
-                            <h6 class="vehicle-title">location</h6>
-                            <p class="vehicle-number">${launchPads[i].location.name}, ${launchPads[i].location.region}</p>
+                            <h6 class="dd-content__title">location</h6>
+                            <p class="dd-content__description">${launchPads[i].location.name}, ${launchPads[i].location.region}</p>
 
-                            <h6 class="vehicle-title">attempted launches</h6>
-                            <p class="vehicle-number">${launchPads[i].attempted_launches}</p>
+                            <h6 class="dd-content__title">attempted launches</h6>
+                            <p class="dd-content__description">${launchPads[i].attempted_launches}</p>
 
-                            <h6 class="vehicle-title">successful launches</h6>
-                            <p class="vehicle-number">${launchPads[i].successful_launches}</p>
+                            <h6 class="dd-content__title">successful launches</h6>
+                            <p class="dd-content__description">${launchPads[i].successful_launches}</p>
 
-                            <h6 class="vehicle-title">details</h6>
-                            <p class="vehicle-number">${launchPads[i].details}</p>
+                            <h6 class="dd-content__title">details</h6>
+                            <p class="dd-content__description">${launchPads[i].details}</p>
                             
                             <a class="wiki" href="${launchPads[i].wikipedia}">wikipedia</a>
                         </div>
