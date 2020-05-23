25d6ecefff15fed23d6f98bd1b2ac3a82c02f780
Emizzari <emilie@tonnessen.com>
2020-05-23 16:55:08 +0200

    Renaming variables

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,7 +3,6 @@ function toogleMobileMenu() {
     var mobileMenu = document.querySelector("#mobile-block-wrap");
     var body = document.querySelector("#body");
 
-
     if (mobileMenu.style.display === "block") {
         mobileMenu.style.display = "none";
         body.style.position = "absolute";
@@ -15,16 +14,16 @@ function toogleMobileMenu() {
 
 
 // Mobile Menu Hide on scolling down - show on scrolling up
-var prevScrollpos = window.pageYOffset;
+let scroll = window.pageYOffset;
 
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
+    const currentScroll = window.pageYOffset;
 
-    if (prevScrollpos > currentScrollPos) {
+    if (scroll > currentScroll) {
         document.getElementById("mobile-scroll-menu").style.bottom = "0";
     } else {
         document.getElementById("mobile-scroll-menu").style.bottom = "-100px";
     }
 
-    prevScrollpos = currentScrollPos;
+    scroll = currentScroll;
 }
